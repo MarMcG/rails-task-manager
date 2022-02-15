af236eb23a31aba434c0b72c12b99451b8684523
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-02-15 14:27:07 -0500

    added view pages & simple form gem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
  def index
   @task = Task.all
  end
@@ -7,9 +9,32 @@ class TasksController < ApplicationController
   @task = Task.new
  end
 
- def show
-  @task = Task.find(params[:id])
+ def show; end
+
+ def create
+  @task = Task.create(task_params)
+  redirect_to task_path(@task)
  end
 
- def
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to task_path
+  end
+
+ private
+
+ def task_params
+  params.require(:task).permit(:title, :details, :completed)
+ end
+
+ def set_task
+  @task = Task.find(params[:id])
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Create ABOVE READ
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
-  # Read(one)
-  get "tasks/:id", to: "tasks#show", as: :task
-  # Read (all)
-  get "tasks", to: "tasks#index"
-  # Update
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
-  # Delete
-  delete "tasks/:id", to: "tasks#destroy"
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
+  # # Read(one)
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # # Read (all)
+  # get "tasks", to: "tasks#index"
+  # # Update
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+  # # Delete
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
